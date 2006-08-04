cd07584dbdd008d47b856c7b6bbbdd0fdb05e1ae
casey.marshall <casey.marshall@7d3312fb-9c19-0410-bd1a-39fa8c26735f>
2006-08-04 04:25:44 +0000

    Added atdf_format static method to AtdfWriter.  Still need to fix how some types are rendered to truly follow the ATDF spec.
    git-svn-id: https://pystdf.googlecode.com/svn/trunk@8 7d3312fb-9c19-0410-bd1a-39fa8c26735f

--- Writers.py
+++ Writers.py
@@ -2,10 +2,17 @@
 import sys, os
 
 class AtdfWriter:
+    @staticmethod
+    def atdf_format(rectype, field_index, value):
+        if value is None:
+            return ''
+        else:
+            return str(value) # TODO: Correct formatting for all types!
+    
     def __init__(self, stream=sys.stdout):
         self.stream = stream
     def after_send(self, dataSource, data):
         self.stream.write('%s:%s%s' % (data[0].__class__.__name__,
-            '|'.join([str(val) for val in data[1]]), os.linesep))
+            '|'.join([self.atdf_format(data[0], i, val) for i, val in enumerate(data[1])]), os.linesep))
     def after_complete(self, dataSource):
         self.stream.flush()

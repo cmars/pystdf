1a1a97eaf909d821501d0d8ebe9897778a9ee670
casey.marshall <casey.marshall@7d3312fb-9c19-0410-bd1a-39fa8c26735f>
2006-08-04 23:26:06 +0000

    Formatting GDR.
    git-svn-id: https://pystdf.googlecode.com/svn/trunk@9 7d3312fb-9c19-0410-bd1a-39fa8c26735f

--- Writers.py
+++ Writers.py
@@ -1,18 +1,24 @@
 
 import sys, os
+from PySTDF import V4
 
 class AtdfWriter:
     @staticmethod
     def atdf_format(rectype, field_index, value):
         if value is None:
-            return ''
+            return ""
+        elif rectype is V4.gdr:
+            return ';'.join([str(v) for v in value])
         else:
             return str(value) # TODO: Correct formatting for all types!
     
     def __init__(self, stream=sys.stdout):
         self.stream = stream
+    
     def after_send(self, dataSource, data):
-        self.stream.write('%s:%s%s' % (data[0].__class__.__name__,
-            '|'.join([self.atdf_format(data[0], i, val) for i, val in enumerate(data[1])]), os.linesep))
+        line = '%s:%s%s' % (data[0].__class__.__name__,
+            '|'.join([self.atdf_format(data[0], i, val) for i, val in enumerate(data[1])]), '\n')
+        self.stream.write(line)
+    
     def after_complete(self, dataSource):
         self.stream.flush()

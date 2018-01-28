9973ec8551f520e70a1ca34ad570c8967d2bbe0e
WinStyle <zhlcanbe@outlook.com>
2018-01-29 01:16:29 +0800

    Update setup.py
    add the stdf2excel and stdf2text in setup.py

--- setup.py
+++ setup.py
@@ -45,8 +45,8 @@ PySTDF is released under a GPL license. Applications developed with PySTDF can o
     author_email='casey.marshall@gmail.com',
     url='http://code.google.com/p/pystdf/',
     packages=['pystdf','pystdf.explorer'],
-    scripts=['scripts/stdf_slice', 'scripts/rec_index',
-             'scripts/stdf2xml'],
+    scripts=['scripts/stdf_slice', 'scripts/rec_index','scripts/stdf2text',
+             'scripts/stdf2excel','scripts/stdf2xml'],
     classifiers=[
       'Development Status :: 4 - Beta',
       'Environment :: Console',
